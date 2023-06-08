91e53b94645249ea56d1a3a38cb56e3721a32c9d
halilcancengiz <hllcncngz1@gmail.com>
2023-06-09 01:50:27 +0300

    updated default toggle value

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import Search from '../components/Search'
 
 
 const Home = () => {
-    const [toggle, setToggle] = useState(false)
+    const [toggle, setToggle] = useState(true)
     return (
         <div style={{ background: `url(https://images.pexels.com/photos/2680270/pexels-photo-2680270.jpeg?cs=srgb&dl=pexels-mudassir-ali-2680270.jpg&fm=jpg)` }} className=' flex items-center justify-center h-screen'>
             <div className='flex flex-col justify-center w-full'>
